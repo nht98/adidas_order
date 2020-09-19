9ad1a2c86f875cb2e5e5897c742da8cbfef9b80a
nht98 <nht98.dev@gmail.com>
2020-09-19 10:42:47 +0700

    api get thông tin đơn jp

--- controller/order_support.js
+++ controller/order_support.js
@@ -1,48 +1,34 @@
 const request = require('request-promise');
 module.exports = {
     getinfo: async (req, res) => {
-        let reid;
-        reurl = /https:\/\/(.+?).adidas/gm;
-        reid = /\/(.+?)\/(.+?)\/(.+?)\/(.+?).html/gm;
-        let pageName = reurl.exec(req.body.url);
-        let idProduct = reid.exec(req.body.url);
-        if (pageName[1] == "www") {
-            let url = "https://www.adidas.com/api/products/" + idProduct[4] + "?sitePath=us";
-            console
-            const getData = {
-                method: 'GET',
-                uri: url
-            };
-            console.log(url);
-            request(getData).then(data_info => {
-                data_info = JSON.parse(data_info);
-                console.log(data_info);
-                res.status(200).json({
-                    message: "Thành công",
-                    data: {
-                        info: {
-                            id_product: data_info.id,
-                            name_product: data_info.name,
-                            image_product: (data_info.product_description.description_assets.image_url) ? data_info.product_description.description_assets.image_url : "",
-                        },
-                    },
-                });
-            }).catch(function () {
-                res.status(400).json({
-                    message: "Thất bại",
-                    data: null
-                });
-            });
-        } else {
+        let url = req.body.url;
+        const getTOken = {
+            method: 'GET',
+            uri: url,
+            headers: {
+                'accept': "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+                'User-Agent': "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.135 Safari/537.36",
+            }
+        };
+        request(getTOken).then(data => {
+            let reName = /"name":"(.+?)","image":/gm;
+            let reImage = /"image":\["(.+?)","/gm;
+            let rePrice = /"price":"(.+?)","/gm;
+            let Image = reImage.exec(data);
+            let Name = reName.exec(data);
+            let Price = rePrice.exec(data);
+            res.status(200).json({
+                data: {
+                    image: Image[1],
+                    name: Name[1],
+                    price: Price[1]
+                }
+            })
+        }).catch(function () {
             res.status(400).json({
                 message: "Thất bại",
                 data: null
             });
-        }
-
-
-    },
-    getstatus: async (req, res) => {
-
+        });
     },
 }
--- routes/support.router.js
+++ routes/support.router.js
@@ -1,5 +1,4 @@
 module.exports = function(app){
     var ctrSupport = require('../controller/order_support.js');
-    app.route('/sp/getinfo').post(ctrSupport.getinfo);
-    app.route('/sp/editorder').post(ctrSupport.getstatus);
+    app.route('/api/getinfo').post(ctrSupport.getinfo);
 }
