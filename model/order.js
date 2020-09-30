162c90c202687944a810e0b7f0313b8038aca2e2
thanhtuancris <thanhtuancristiano@gmail.com>
2020-10-01 02:18:44 +0700

    fix edit_order

--- model/order.js
+++ model/order.js
@@ -7,7 +7,7 @@ const orderSchema = new mongoose.Schema({
     realquantity: Number,
     address_ship : String,
     image : String,
-    data_order : Date,
+    date_order : Date,
     nation : String,
     idShiper : mongoose.Types.ObjectId,
     nameProduct : String,
