23ff7d3ed2298d57e502a8190e98136e200668f9
thanhtuancris <thanhtuancristiano@gmail.com>
2020-10-01 08:50:40 +0700

    update date and time

--- model/order.js
+++ model/order.js
@@ -7,7 +7,7 @@ const orderSchema = new mongoose.Schema({
     realquantity: Number,
     address_ship : String,
     image : String,
-    date_order : String,
+    data_order : String,
     nation : String,
     idShiper : mongoose.Types.ObjectId,
     nameProduct : String,
