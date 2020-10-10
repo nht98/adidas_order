f0013eeee9e8caac0f87deb12d8bd0201f063648
thanhtuancris <thanhtuancris@localhost.localdomain>
2020-10-10 14:07:14 +0700

    update order_support

--- controller/order_support.js
+++ controller/order_support.js
@@ -1,18 +1,16 @@
-// const request = require('request-promise');
+const rp = require('request-promise');
 const request = require('request');
 // var http = new chilkat.Http();
 module.exports = {
-    getinfo: (req, res) => {
-        const rePage = /https:\/\/(.+?)\//gm
+    getinfo_jp: async (req, res) => {
+        var data = 0;
+        // const rePage = /https:\/\/(.+?)\//gm;
         let reName = /"name":"(.+?)","image":/gm;
         let reImage = /"image":\["(.+?)","/gm;
         let rePrice = /"price":"(.+?)","/gm;
-        let Image = reImage.exec(data);
-        let Name = reName.exec(data);
-        let Price = rePrice.exec(data);
         let url = req.body.url;
-        let host = rePage.exec(url);
-        request.get({
+        // let host = rePage.exec(url);
+        let options = {
             uri: url,
             headers: {
                 'Host':'shop.adidas.jp',
@@ -25,14 +23,108 @@ module.exports = {
                 'TE':'Trailers',
                 'Upgrade-Insecure-Requests':'1'
             }
-        },function(err, res, body) {
+        };
+        await rp.post(options, function(err, res, body) {
             if(err) {
-                return console.log(err);
+                console.log(err);
             }else{
-                console.log(body);
+                data = body;
             }
         });
+        let Image = reImage.exec(data);
+        let Name = reName.exec(data);
+        let Price = rePrice.exec(data);
+        res.status(200).json({
+            data:
+                {     
+                    image: Image[1],
+                    name: Name[1],
+                    price: Price[1]
+                }
+        });
+    },
 
+    getinfo_usa: async (req, res) => {
+        var data = 0;
+        let reName = /"name":"(.+?)","color"/gm;
+        let reImage = /"image":\["(.+?)","/gm;
+        let rePrice = /"price":(.+?)}}/gm;
+        let url = req.body.url;
+        let options = {
+            uri: url,
+            headers: {
+                'Host': 'www.adidas.com',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0',
+                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Content-Type': 'application/json',
+                'Accept-Encoding':'',
+                'Connection':'keep-alive',
+                'TE':'Trailers',
+                'Upgrade-Insecure-Requests':'1'
+            }
+        };
+        await rp.get(options, function(err, res, body) {
+            if(err) {
+                console.log(err);
+            }else{
+                data = body;
+            }
+        });
+       
+        let Image = reImage.exec(data);
+        let Name = reName.exec(data);
+        let Price = rePrice.exec(data);
+        res.status(200).json({
+            data:
+                {     
+                    image: Image[1],
+                    name: Name[1],
+                    price: Price[1]
+                }
+        });
+    },
+
+    getinfo_ger: async (req, res) => {
+        var data = 0;
+        let reName = /"name":"(.+?)","color"/gm;
+        let reImage = /"image":\["(.+?)","/gm;
+        let rePrice = /"price":(.+?)}}/gm;
+        let url = req.body.url;
+        let options = {
+            uri: url,
+            headers: {
+                'Host': 'www.adidas.de',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:68.0) Gecko/20100101 Firefox/68.0',
+                'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Content-Type': 'application/json',
+                'Accept-Encoding':'',
+                'Connection':'keep-alive',
+                'TE':'Trailers',
+                'Upgrade-Insecure-Requests':'1'
+            }
+        };
+        await rp.get(options, function(err, res, body) {
+            if(err) {
+                console.log(err);
+            }else{
+                data = body;
+            }
+        });
+        let Image = reImage.exec(data);
+        let Name = reName.exec(data);
+        let Price = rePrice.exec(data);
+        res.status(200).json({
+            data:
+                {     
+                    image: Image[1],
+                    name: Name[1],
+                    price: Price[1]
+                }
+        });
+    },
+}
         // try{ 
         //     var nation = rePage.exec(url);
         //     nation = nation[1];
@@ -73,5 +165,5 @@ module.exports = {
         //         message: ex
         //     })
         // }
-    },
-}
+//     },
+// }
