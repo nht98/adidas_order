373ff34f4d174f116dbe9d7526f66da976b396bd
thanhtuancris <thanhtuancristiano@gmail.com>
2020-12-14 09:08:13 +0700

    update pusher

--- model/token_noti.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose")
-
-const token_notiSchema = new mongoose.Schema({
-    id_account: mongoose.Types.ObjectId,
-    token: String,
-    date_created: Date,
-})
-
-module.exports = mongoose.model("token_noti", token_notiSchema)
--- routes/notification.router.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(app){
-    var ctrNotification = require('../controller/notification_controller');
-    app.route('/api/add-notification').post(ctrNotification.add_notification);
-    app.route('/api/add-token-notification').post(ctrNotification.add_token_notification);
-
-}
